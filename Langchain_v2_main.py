fab0758c392ee130f50da660544dc21761cec4e6
Alphteow <e0540502@u.nus.edu>
2025-02-10 12:26:06 +0800

    Added version2

---
+++ Langchain_v2_main.py
@@ -0,0 +1,91 @@
+# from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import openai 
+from dotenv import load_dotenv
+import os
+import shutil
+
+# Load environment variables. Assumes that project contains .env file with API keys
+load_dotenv()
+#---- Set OpenAI API key 
+# Change environment variable name from "OPENAI_API_KEY" to the name given in 
+# your .env file.
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "corpus/pdf"
+
+
+def main():
+    generate_data_store()
+
+
+def generate_data_store():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+
+
+def load_documents():
+    # Directory containing PDF and CSV files
+    data_directory = "corpus"
+
+    # Initialize a list to store all documents
+    documents = []
+
+    # Process PDF files
+    print("Loading PDF documents...")
+    pdf_directory = os.path.join(data_directory, "pdf")
+    for filename in os.listdir(pdf_directory):
+        if filename.endswith(".pdf"):
+            file_path = os.path.join(pdf_directory, filename)
+            loader = PyPDFLoader(file_path)  # Load PDF with metadata (filename, page number)
+            documents.extend(loader.load())
+
+    # Process CSV files
+    print("Loading CSV documents...")
+    csv_directory = os.path.join(data_directory, "csv")
+    for filename in os.listdir(csv_directory):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(csv_directory, filename)
+            loader = CSVLoader(file_path)  # Load CSV with metadata (filename, row number)
+            documents.extend(loader.load())
+    return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=500,
+    length_function=len,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[1]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
+
+
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Create a new DB from the documents.
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+
+if __name__ == "__main__":
+    main()
